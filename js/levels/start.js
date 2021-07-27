439dd4dfddbfd896da5b5149c088d743875d14d3
matthewproskils <matthewproskils@gmail.com>
2021-07-27 13:47:26 -0700

    Collision With Map Finished

--- js/levels/start.js
+++ js/levels/start.js
@@ -1,41 +1,10 @@
 import { drawPlayer, drawTilemap } from '../draw.js';
 import { LoadImage } from '../util.js';
 import colors from '../colors.js';
+import { Movement } from '../collision.js';
 let Player;
 let TilemapImg;
 let Tilemap;
-function Movement(PlayerStatus, keyPressed, deltaTime, nextFrameAnimation) {
-    if (keyPressed['left'] || keyPressed['ArrowLeft']) {
-        PlayerStatus.x -= PlayerStatus.speed * (deltaTime / 1000);
-        PlayerStatus.facing = 4;
-    }
-    else if (keyPressed['right'] || keyPressed['ArrowRight']) {
-        PlayerStatus.x += PlayerStatus.speed * (deltaTime / 1000);
-        PlayerStatus.facing = 3;
-    }
-    else if (keyPressed['up'] || keyPressed['ArrowUp']) {
-        PlayerStatus.y -= PlayerStatus.speed * (deltaTime / 1000);
-        PlayerStatus.facing = 1;
-    }
-    else if (keyPressed['down'] || keyPressed['ArrowDown']) {
-        PlayerStatus.facing = 2;
-        PlayerStatus.y += PlayerStatus.speed * (deltaTime / 1000);
-    }
-    if (nextFrameAnimation) {
-        if ((keyPressed['down'] || keyPressed['ArrowDown']) || (keyPressed['up'] || keyPressed['ArrowUp']) || (keyPressed['right'] || keyPressed['ArrowRight']) || (keyPressed['left'] || keyPressed['ArrowLeft'])) {
-            if (PlayerStatus.position == 3) {
-                PlayerStatus.position = 1;
-            }
-            else {
-                PlayerStatus.position++;
-            }
-        }
-        else {
-            PlayerStatus.position = 0;
-        }
-    }
-    return PlayerStatus;
-}
 export default async function Start(ctx, gameStats, globalStats, deltaTime, keyPressed) {
     if (gameStats === null) {
         gameStats = {
@@ -62,7 +31,7 @@ export default async function Start(ctx, gameStats, globalStats, deltaTime, keyP
     gameStats.nextFrameAnimation == 10 ? gameStats.nextFrameAnimation = 0 : gameStats.nextFrameAnimation++;
     ctx.fillStyle = colors[2];
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    gameStats.PlayerStatus = Movement(gameStats.PlayerStatus, keyPressed, deltaTime, gameStats.nextFrameAnimation == 4);
+    gameStats.PlayerStatus = Movement(gameStats.PlayerStatus, keyPressed, deltaTime, gameStats.nextFrameAnimation == 4, Tilemap.data);
     drawTilemap(Tilemap.width, Tilemap.height, Tilemap.data, TilemapImg, ctx);
     drawPlayer(gameStats.PlayerStatus.x, gameStats.PlayerStatus.y, gameStats.PlayerStatus.facing, gameStats.PlayerStatus.position, Player, ctx);
     return gameStats;
--- ts/collision.ts
+++ ts/collision.ts
@@ -1,16 +1,75 @@
 export interface rect {
     width: number,
-    heigt: number,
+    height: number,
     x: number,
     y: number,
 }
 
-//COllision detection which takes 2 rects
-
-export function collision(rect1: number, rect2: number){
-
+export function collision(rect1: rect, rect2: rect){
+    return !(rect1.x + rect1.width < rect2.x || rect1.x > rect2.x + rect2.width || rect1.y + rect1.height < rect2.y || rect1.y > rect2.y + rect2.height);
 }
 
-export function collisionMap() {
-
+export function collisionMap(map: Array<number>, character: rect) {
+    for (let i = 0; i < map.length; i++) {
+        if (map[i] === 5) {
+            if (collision(character, {
+                width: 1,
+                height: 1,
+                x: i % 8,
+                y: Math.floor(i/8),
+            })) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
+
+export function Movement(PlayerStatus: any, keyPressed: { [key: string]: boolean }, deltaTime: number, nextFrameAnimation: boolean, tilemapData: Array<number>) {
+    let hasMoved = false;
+    if (keyPressed['left'] || keyPressed['ArrowLeft']) {
+      PlayerStatus.x -= PlayerStatus.speed * (deltaTime / 1000);
+      if(collisionMap(tilemapData, {x: PlayerStatus.x, y: PlayerStatus.y, width: 1, height: 1})) {
+        PlayerStatus.x += PlayerStatus.speed * (deltaTime / 1000);
+      } else {
+        PlayerStatus.facing = 4;
+        hasMoved = true;
+      }
+    } else if (keyPressed['right'] || keyPressed['ArrowRight']) {
+      PlayerStatus.x += PlayerStatus.speed * (deltaTime/1000);
+      if(collisionMap(tilemapData, {x: PlayerStatus.x, y: PlayerStatus.y, width: 1, height: 1})) {
+        PlayerStatus.x -= PlayerStatus.speed * (deltaTime / 1000);
+      } else {
+        PlayerStatus.facing = 3;
+        hasMoved = true;
+      }
+    } else if (keyPressed['up'] || keyPressed['ArrowUp']) {
+      PlayerStatus.y -= PlayerStatus.speed * (deltaTime/1000);
+      if(collisionMap(tilemapData, {x: PlayerStatus.x, y: PlayerStatus.y, width: 1, height: 1})) {
+        PlayerStatus.y += PlayerStatus.speed * (deltaTime / 1000);
+      } else {
+        PlayerStatus.facing = 1;
+        hasMoved = true;
+      }
+    } else if (keyPressed['down'] || keyPressed['ArrowDown']) {
+      PlayerStatus.y += PlayerStatus.speed * (deltaTime/1000);
+      if(collisionMap(tilemapData, {x: PlayerStatus.x, y: PlayerStatus.y, width: 1, height: 1})) {
+        PlayerStatus.y -= PlayerStatus.speed * (deltaTime / 1000);
+      } else {
+        PlayerStatus.facing = 2;
+        hasMoved = true;
+      }
+    }
+    if (nextFrameAnimation) {
+      if (hasMoved) {
+        if (PlayerStatus.position == 3) {
+          PlayerStatus.position = 1;
+        } else {
+          PlayerStatus.position++;
+        }
+      } else {
+        PlayerStatus.position = 0;
+      }
+    }
+    return PlayerStatus;
+  }
