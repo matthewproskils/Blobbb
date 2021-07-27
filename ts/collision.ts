5daf2ba78424638fa0a981d884b01a94d7a76932
matthewproskils <matthewproskils@gmail.com>
2021-07-27 12:20:31 -0700

    collision

---
+++ ts/collision.ts
@@ -0,0 +1,16 @@
+export interface rect {
+    width: number,
+    heigt: number,
+    x: number,
+    y: number,
+}
+
+//COllision detection which takes 2 rects
+
+export function collision(rect1: number, rect2: number){
+
+}
+
+export function collisionMap() {
+
+}
